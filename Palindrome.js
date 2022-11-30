ff524ad6042357898351a2bdcda2a6bc507ba1e0
vaishnavi kulkarni <vaishnavikulkarni124@gmailcom>
2022-11-30 18:27:58 +0530

    added the programs

---
+++ Palindrome.js
@@ -0,0 +1,13 @@
+ 
+let str="NAMAN"
+let bag ="";
+    for ( let i = str.length-1 ; i>=0 ; i++){
+        bag += str[i]
+    }
+    if (bag == str[i]){
+        console.log("Yes")
+    }else {
+        console.log("No")
+    }
+
+correct the for loop(i--) and if condition(str) 
---
+++ Prime.js
@@ -0,0 +1,14 @@
+ let count=1
+   for (let i=1 ; i<num ; i++){
+       if(num % i ==1){
+           count++
+       }
+   }
+   
+   if (count==0){
+       console.log("Yes")
+   }else {
+       console.log("No")
+   }
+
+*correctc code with two if condition *
